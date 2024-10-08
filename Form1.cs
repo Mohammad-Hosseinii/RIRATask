6fc4f008b51940e27b28da8b4d6aec9082260d54
Mohammad <m.hosseiniiii79@gmail.com>
2024-10-08 09:36:49 +0330

    ImplementationOfRIRATaskInGitHub

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Test.Product;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Product> products = new List<Product>
+        {
+           new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
+           new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
+           new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
+           new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
+           new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            listBox1.DataSource = null;
+            listBox1.DataSource = p.GetProducts(products);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            listBox1.DataSource = null;
+            MessageBox.Show("Total prices is:" + " " + p.Total(products).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            listBox1.DataSource = null;
+            MessageBox.Show("Maximum price is:" + " " + p.GetMaxPrice(products).ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            listBox1.DataSource = null;
+            listBox1.DataSource = p.Grouping(products);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            listBox1.DataSource = null;
+            MessageBox.Show("Average prices is:" + " " + p.Average(products).ToString());
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Categories Category { get; set; }
+        public int Price { get; set; }
+
+        public enum Categories
+        {
+            Category1,
+            Category2,
+            Category3,
+        }
+
+        //I used lambda in task 2, 3 and 5 to do my job.
+        //Task1
+        //Get product by kind of category.
+        public List<string> GetProducts(List<Product> productList)
+        {
+            var productName = from i in productList where i.Category == Categories.Category1 select i.Name;
+            return productName.ToList();
+        }
+
+        //Task2
+        public int GetMaxPrice(List<Product> productList)
+        {
+            var maxPrice = productList.Max(i => i.Price);
+            return maxPrice;
+        }
+
+        //Task3
+        public int Total(List<Product> productList)
+        {
+            var totalPrice = productList.Sum(i => i.Price);
+            return totalPrice;
+        }
+
+        //Task4
+        public List<string> Grouping(List<Product> productList)
+        {
+            var q = from i in productList
+                    group i by i.Category;
+            List<string> groupList = new List<string>();
+            foreach (var group in q)
+            {
+                //MessageBox.Show($"Grade {group.Key}:");
+                groupList.Add(group.Key.ToString());
+                foreach (var product in group)
+                {
+                    //MessageBox.Show($" {product.Name}");
+                    groupList.Add(product.Name.ToString());
+                }
+                groupList.Add("**********");
+            }
+            return groupList;
+        }
+
+        //Task5
+        public double Average(List<Product> productList)
+        {
+            var avg = productList.Average(i => i.Price);
+            return avg;
+        }
+    }
+}
